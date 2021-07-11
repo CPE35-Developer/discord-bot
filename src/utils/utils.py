12ca84e9636dd473acffed1535d72f3f9175847d
Phusit Somboonyingsuk <phusit.so@ku.th>
2021-07-11 20:44:31 +0700

    Restructure folders and create guess_command

---
+++ src/utils/utils.py
@@ -0,0 +1,14 @@
+from difflib import SequenceMatcher
+
+commands = ['$hello', '$poker']
+
+async def guess_command(client, message):
+    similar_commands = []
+    if (message.content.startswith('$')) & (message.content not in commands):
+        print(f'{message.author}: {message.content}')
+        for command in commands:
+            similar_ratio = SequenceMatcher(None, message.content, command).ratio()
+            if similar_ratio >= 0.6:
+                similar_commands.append([command, similar_ratio])
+        similar_commands = sorted(similar_commands,key=lambda l:l[1], reverse=True)
+        await message.channel.send(f'คุณกำลังจะพิมพ์ {similar_commands[0][0]} หรือเปล่า')
