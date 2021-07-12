efe50ab97f009b53856d2aec0870c0c664b19da5
namirinz <nathmcfc31115@gmail.com>
2021-07-12 23:56:25 +0700

    update kick.py (emoji snap)

--- src/utils/kick.py
+++ src/utils/kick.py
@@ -1,9 +1,8 @@
-from termios import INLCR
 from discord.ext.commands import MemberConverter
 from random import sample, random
 
 BOT_ID = 863101912730959894
-GENERAL_VOICE_ID = 847172394316464184
+THANOS_EMOJI_ID = 864186198183116831
 
 async def kick_person(user):
   await user.move_to(None)
@@ -15,10 +14,16 @@ async def random_kick(bot, ctx, user):
       await ctx.send(f'โชคร้ายหน่อยนะ {str(user)} บายย')
       await user.move_to(None)
     else:
-      await ctx.send(f'โชคดีไป {str(user)}')
+      await ctx.send(f'โชคดีไป {str(user)}') #choose only 1 line between 18 or 19 or keep both?
+      
+      if user != ctx.author:
+        await ctx.send(f'มึงโดนแทนละกั้น {str(ctx.author)}')
+        await ctx.author.move_to(None)
 
   else:
-    general_channel = bot.get_channel(GENERAL_VOICE_ID)  
+    await ctx.send(f':thanossnap128:')
+
+    general_channel = ctx.author.voice.channel
     member_ids = list(general_channel.voice_states.keys())
       
     random_member_ids = sample(member_ids, (len(member_ids)//2)+1)
@@ -32,3 +37,4 @@ async def random_kick(bot, ctx, user):
       print(str(player))
 
       await player.move_to(None)
+    await ctx.send(f'Perfectly balanced, as all things should be')
