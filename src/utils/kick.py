21edf8734216af3873371e35e7497a723957dc2a
jiratQ <64889751+jiratQ@users.noreply.github.com>
2021-07-12 19:42:51 +0700

    change snap @person  (now can kick author) (#37)

--- src/utils/kick.py
+++ src/utils/kick.py
@@ -15,7 +15,9 @@ async def random_kick(bot, ctx, user):
       await ctx.send(f'โชคร้ายหน่อยนะ {str(user)} บายย')
       await user.move_to(None)
     else:
-      await ctx.send(f'โชคดีไป {str(user)}')
+      await ctx.send(f'โชคดีไป {str(user)}') #choose only 1 line between 18 or 19 or keep both?
+      await ctx.send(f'มึงโดนแทนละกั้น {str(ctx.author)}')
+      await ctx.author.move_to(None)
 
   else:
     general_channel = bot.get_channel(GENERAL_VOICE_ID)  
