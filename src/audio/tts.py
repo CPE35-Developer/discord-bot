8882a530324dab23374cbe2d905f14ffb89e9880
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-07-13 12:36:29 +0700

    Add try catch os error

--- src/audio/tts.py
+++ src/audio/tts.py
@@ -2,7 +2,8 @@ from gtts import gTTS
 from discord import FFmpegPCMAudio
 from langdetect import detect
 from src.utils.vc import get_PATH_ffmpeg
-import sys
+from src.utils.utils import config
+
 PATH_ffmpeg =  get_PATH_ffmpeg()
 
 async def repeat(vc, text=None):
@@ -10,12 +11,11 @@ async def repeat(vc, text=None):
         language = detect(text)
     except:
         language = 'en'
-        
+
     tts = gTTS(text=text, lang=language)
-    
-    try: 
+    try:
         tts.save("text.mp3")
         vc.play(FFmpegPCMAudio(executable=PATH_ffmpeg, source='text.mp3'))
-    except: 
+    except:
         tts.save("C:/text.mp3")
-        vc.play(FFmpegPCMAudio(executable=PATH_ffmpeg, source='C:/text.mp3'))
+        vc.play(FFmpegPCMAudio(executable=config.audio.PATH_ffmpeg_windows, source='C:/text.mp3'))
