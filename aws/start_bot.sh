a40d25ff2cf34c01f535349cd0ebb23456cab861
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-07-19 14:35:57 +0700

    update

--- aws/start_bot.sh
+++ aws/start_bot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 pip3 install --upgrade awscli
-aws s3 cp s3://cpe35-discord-bot/.env  ~/.env
+
+$(aws s3 cp s3://cpe35-discord-bot/.env  ~/.env) &
 sudo mv ~/.env /deploy/discord-bot
+
 cd /deploy/discord-bot
 pip install -r requirements.txt
 forever start -c ~/adam/envs/discord/bin/python main.py 
