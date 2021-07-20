d3bfd8ee1bb3e0d19044119fcaf32c2d54bb18e7
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-07-21 00:21:35 +0700

    Added pog

---
+++ src/pog/utils.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+
+
+DECKS_OF_CARDS = [
+    ':two: :clubs:', ':two: :diamonds:', ':two: :heart:', ':two: :spades:',
+    ':three: :clubs:', ':three: :diamonds:', ':three: :heart:', ':three: :spades:',
+    ':four: :clubs:', ':four: :diamonds:', ':four: :heart:', ':four: :spades:',
+    ':five: :clubs:', ':five: :diamonds:', ':five: :heart:', ':five: :spades:',
+    ':six: :clubs:', ':six: :diamonds:', ':six: :heart:', ':six: :spades:',
+    ':seven: :clubs:', ':seven: :diamonds:', ':seven: :heart:', ':seven: :spades:',
+    ':eight: :clubs:', ':eight: :diamonds:', ':eight: :heart:', ':eight: :spades:',
+    ':nine: :clubs:', ':nine: :diamonds:', ':nine: :heart:', ':nine: :spades:',
+    ':one::zero: :clubs:', ':one::zero: :diamonds:', ':one::zero: :heart:', ':one::zero: :spades:',
+    ':regional_indicator_j: :clubs:', ':regional_indicator_j: :diamonds:', ':regional_indicator_j: :heart:', ':regional_indicator_j: :spades:',
+    ':regional_indicator_q: :clubs:', ':regional_indicator_q: :diamonds:', ':regional_indicator_q: :heart:', ':regional_indicator_q: :spades:',
+    ':regional_indicator_k: :clubs:', ':regional_indicator_k: :diamonds:', ':regional_indicator_k: :heart:', ':regional_indicator_k: :spades:',
+    ':a: :clubs:', ':a: :diamonds:', ':a: :heart:', ':a: :spades:'
+]
+
+
