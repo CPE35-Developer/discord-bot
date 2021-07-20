85b3a0b8b7c0fb49a408ff8d762c8e882d82578a
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-07-20 22:09:20 +0700

    sudooo

--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -2,4 +2,4 @@
 aws s3 cp s3://cpe35-discord-bot/.env /deploy/discord-bot/.env
 cd /deploy/discord-bot
 pip install -r requirements.txt
-nohup /home/ubuntu/adam/envs/discord/bin/python main.py > /dev/null 2> /dev/null < /dev/null &
+sudo nohup /home/ubuntu/adam/envs/discord/bin/python main.py > /dev/null 2> /dev/null < /dev/null &
